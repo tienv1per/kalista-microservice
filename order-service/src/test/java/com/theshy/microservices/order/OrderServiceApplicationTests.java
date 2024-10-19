381a19ace98636eea7a7b4a378c4a9930e55bfab
tienv1per <tienv1perds@gmail.com>
2024-10-19 15:44:15 +0700

    integration test for order service

--- order-service/src/test/java/com/theshy/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/theshy/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.theshy.microservices.order;
 
+import io.restassured.RestAssured;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:8.3.0"));
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+		RestAssured.baseURI = "http://localhost";
+	}
+
+	static {
+		mySQLContainer.start();
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+			{
+				"skuCode": "Macbook_Pro_M1",
+				"price": 1111,
+				"quantity": 111
+			}
+		""";
+
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		MatcherAssert.assertThat(responseBodyString, Matchers.is("Order Placed successfully"));
 	}
 
 }

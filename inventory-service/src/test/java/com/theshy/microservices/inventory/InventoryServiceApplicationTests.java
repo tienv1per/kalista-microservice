8973a637b18418c35159030f92aaaced728e946f
tienv1per <tienv1perds@gmail.com>
2024-10-19 23:31:13 +0700

    add integration test for inventory service

--- inventory-service/src/test/java/com/theshy/microservices/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/theshy/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,53 @@
 package com.theshy.microservices.inventory;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:8.3.0"));
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+		RestAssured.baseURI = "http://localhost";
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldReadInventory() {
+		var response = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone_15&quantity=50")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		Assertions.assertTrue(response);
+
+		var negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone_15&quantity=150")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		Assertions.assertFalse(negativeResponse);
+	}
 }

cc0b604105ef66393a0bb5fe9e3d64afef76773d
tienv1per <tienv1perds@gmail.com>
2024-10-20 00:48:30 +0700

    implement open feign to make http request from order service to inventory service

--- order-service/src/main/java/com/theshy/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/theshy/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.theshy.microservices.order.service;
 
+import com.theshy.microservices.order.client.InventoryClient;
 import com.theshy.microservices.order.dto.OrderRequest;
 import com.theshy.microservices.order.model.Order;
 import com.theshy.microservices.order.repository.OrderRepository;
@@ -12,16 +13,22 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         // map order request to order object
-        Order order = Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .price(orderRequest.price())
-                .skuCode(orderRequest.skuCode())
-                .quantity(orderRequest.quantity())
-                .build();
-        // save order
-        orderRepository.save(order);
+        if(isProductInStock) {
+            Order order = Order.builder()
+                    .orderNumber(UUID.randomUUID().toString())
+                    .price(orderRequest.price())
+                    .skuCode(orderRequest.skuCode())
+                    .quantity(orderRequest.quantity())
+                    .build();
+            // save order
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }

3b9538a04379b3fefab548b6f216537297d1e734
tienv1per <tienv1perds@gmail.com>
2024-10-22 23:25:40 +0700

    add kafka demo to order service

--- order-service/src/main/java/com/theshy/microservices/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/theshy/microservices/order/dto/OrderRequest.java
@@ -2,11 +2,8 @@ package com.theshy.microservices.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(
-        Long id,
-        String orderNumber,
-        String skuCode,
-        BigDecimal price,
-        Integer quantity
-) {
+public record OrderRequest(Long id, String orderNumber, String skuCode,
+                           BigDecimal price, Integer quantity, UserDetails userDetails) {
+
+    public record UserDetails(String email, String firstName, String lastName) {}
 }
--- order-service/src/main/java/com/theshy/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/theshy/microservices/order/service/OrderService.java
@@ -2,9 +2,13 @@ package com.theshy.microservices.order.service;
 
 import com.theshy.microservices.order.client.InventoryClient;
 import com.theshy.microservices.order.dto.OrderRequest;
+import com.theshy.microservices.order.event.OrderPlacedEvent;
 import com.theshy.microservices.order.model.Order;
 import com.theshy.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -12,8 +16,10 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -27,6 +33,12 @@ public class OrderService {
                     .build();
             // save order
             orderRepository.save(order);
+
+            // send message to kafka topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), "vanhoang210202@gmail.com");
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
